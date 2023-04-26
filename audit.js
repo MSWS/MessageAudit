3519657a20e440ea8d054a20d8d06229f7fc02b3
MSWS <imodmaker@gmail.com>
2023-04-25 22:57:24 -0700

    Add spreadsheet compatability

--- audit.js
+++ audit.js
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
-// import * as dotenv from 'dotenv';
+import { google } from 'googleapis';
 
-// dotenv.config();
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 
 const servers = [
     ['http://edgegamers.gameme.com/chat/csgo2', 'TTT'],
@@ -16,8 +17,15 @@ const servers = [
 const ai_moderation = "https://api.openai.com/v1/moderations";
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 const RUN_RATE = parseInt(process.env.RUN_RATE);
+const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+const RANGE_NAME = 'Sheet1';
 
-// async function main() {
+const auth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+});
+
+const sheets = google.sheets({ version: 'v4', auth });
+
 export const handler = async (event) => {
     const parsers = [];
     for (const server of servers) {
@@ -59,6 +67,21 @@ export const handler = async (event) => {
             const trimmedMessages = messages.slice(start, end).map((message) => message.user + ": " + message.message).join('\n');
             const hook = generateWebhook(oldCategories[0], server[1], trimmedMessages, users);
             await sendWebhook(hook);
+
+            // Add the message to the Google Sheets spreadsheet
+            const values = [
+                [new Date(), server[1], oldCategories[0], trimmedMessages]
+            ];
+            await sheets.spreadsheets.values.append({
+                spreadsheetId: SPREADSHEET_ID,
+                range: RANGE_NAME,
+                valueInputOption: 'USER_ENTERED',
+                resource: {
+                    values: values
+                },
+                insertDataOption: 'INSERT_ROWS'
+
+            });
         });
     }
     await Promise.all(parsers.map((parser) => parser()));
@@ -93,7 +116,6 @@ function parseMessages(text) {
         }
         const date = Date.parse(result.time);
         const now = Date.now();
-        console.log(now + parseInt(process.env.TIME_OFFSET) - date, RUN_RATE * 1000);
         if (now + parseInt(process.env.TIME_OFFSET) - date > RUN_RATE * 1000)
             break;
         messages.push(result);
@@ -112,6 +134,7 @@ async function checkFlag(text) {
             "input": text
         })
     });
+    // console.log(await request.text())
     const response = await request.json();
     const result = response.results[0];
     if (!result.flagged)
@@ -156,15 +179,15 @@ function generateWebhook(category, server, messages, users) {
             {
                 "author": {
                     "name": `${titleCased} Content - ${server}`,
-                },
-                "description": `\`\`\`${messages}\`\`\``,
-                "color": color,
-                "fields": [
-                    {
-                        "name": "Users",
-                        "value": users,
-                    }
-                ]
+                    "description": `\`\`\`${messages}\`\`\``,
+                    "color": color,
+                    "fields": [
+                        {
+                            "name": "Users",
+                            "value": users,
+                        }
+                    ]
+                }
             }
         ]
     }
